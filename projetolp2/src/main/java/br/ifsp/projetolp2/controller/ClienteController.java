63ae81aea843269dac8cf0d8afff21cbf71418ec
Matheus Claudino Ribeiro <74937108+C4mat@users.noreply.github.com>
2023-05-29 13:07:53 -0300

    Add files via upload

--- projetolp2/src/main/java/br/ifsp/projetolp2/controller/ClienteController.java
+++ projetolp2/src/main/java/br/ifsp/projetolp2/controller/ClienteController.java
@@ -1,16 +1,36 @@
 package br.ifsp.projetolp2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.ifsp.projetolp2.cliente.Cliente;
+import br.ifsp.projetolp2.cliente.ClienteRepository;
+import br.ifsp.projetolp2.cliente.ClienteRequestDTO;
+import br.ifsp.projetolp2.cliente.ClienteResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("cliente")
 public class ClienteController {
-   @GetMapping
-    public void getAll(){
 
+    @Autowired
+    private ClienteRepository repository;
+    @CrossOrigin (origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveCliente(@RequestBody ClienteRequestDTO data){
+        Cliente clienteData = new Cliente(data);
+        repository.save(clienteData);
+        return;
+    }
+
+    @GetMapping
+    public List<ClienteResponseDTO> getAll(){
+
+        List<ClienteResponseDTO> clienteList = repository.findAll().stream().map(ClienteResponseDTO::new).toList();
+        return clienteList;
+    }
 
 
     }
-}
+
